29e62ef395441bc93f382c9fd4648ebed3c01497
David Heinemeier Hansson <david@hey.com>
2026-05-30 18:27:57 +0200

    Fix native question

--- install.sh
+++ install.sh
@@ -21,14 +21,22 @@ is_arch() {
 }
 
 install_natively_on_arch() {
-  local answer
+  local answer=""
 
   if ! is_arch; then
     return 1
   fi
 
-  read -r -p "Install natively or via Docker? [N/d] " answer </dev/tty
-  [[ ! "$answer" =~ ^[Dd]([Oo][Cc][Kk][Ee][Rr])?$ ]]
+  if [ -r /dev/tty ]; then
+    read -r -p "Install natively or via Docker? [N/d] " answer </dev/tty || true
+  else
+    read -r -p "Install natively or via Docker? [N/d] " answer || true
+  fi
+
+  case "$answer" in
+    [Dd] | [Dd][Oo][Cc][Kk][Ee][Rr]) return 1 ;;
+    *) return 0 ;;
+  esac
 }
 
 run_installer() {
